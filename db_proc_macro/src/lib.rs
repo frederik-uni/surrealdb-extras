f5546b47e842e1f353e17f948a6fb83ef9378c57
Frederik <frederik@MacBook-Pro-von-Frederik.local>
2024-01-30 11:25:51 +0100

    Initial commit

---
+++ db_proc_macro/src/lib.rs
@@ -0,0 +1,42 @@
+mod part;
+mod table;
+
+use proc_macro::TokenStream;
+use std::collections::HashMap;
+use std::fmt::{Debug, Display};
+
+use quote::quote;
+use std::fs::File;
+use std::io::Write;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
+
+#[derive(deluxe::ExtractAttributes, deluxe::ParseAttributes)]
+#[deluxe(attributes(serde))]
+struct SerdeRename {
+    rename: String,
+}
+
+//TODO: apply rename to serde
+#[derive(deluxe::ExtractAttributes, deluxe::ParseAttributes)]
+#[deluxe(attributes(opt))]
+struct SurrealTableOverwrite {
+    rename: Option<String>,
+    db_type: Option<String>,
+    exclude: Option<bool>
+}
+
+#[derive(deluxe::ExtractAttributes)]
+#[deluxe(attributes(db))]
+struct SurrealDatabaseName(String);
+
+#[proc_macro_derive(SurrealSelect, attributes(opt))]
+/// implements SurrealSelectInfo
+pub fn select(input: TokenStream) -> TokenStream {
+    part::derive_attribute_collector(input)
+}
+
+#[proc_macro_derive(SurrealTable, attributes(db, opt))]
+/// implements SurrealSelectInfo, SurrealTableInfo, add and insert
+pub fn table(input: TokenStream) -> TokenStream {
+    table::derive_attribute_collector(input)
+}
---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+#![doc=include_str!( "../readme.md")]
+mod define;
+mod does_imp;
+mod r2k;
+mod records;
+mod surreal_table;
+mod thing;
+mod thing_distplay;
+use std::collections::HashMap;
+use serde::de::DeserializeOwned;
+
+pub use db_proc_macro::SurrealSelect;
+pub use db_proc_macro::SurrealTable;
+pub use define::use_ns_db;
+pub use records::Record;
+pub use records::RecordData;
+pub use surreal_table::SurrealTableInfo;
+pub use thing::ThingFunc;
+pub use thing::ThingType;
+
+
+
+#[doc(hidden)]
+/// converts struct structure to the db type
+/// is used by SurrealTableInfo
+pub fn rust_to_surreal(s: &str, names: &HashMap<&'static str, &'static str>) -> String {
+    r2k::to_kind(s, names).to_string()
+}
+
+/// SELECT {keys} IN db
+pub trait SurrealSelectInfo: DeserializeOwned {
+    /// all attributes
+    fn keys() -> &'static [&'static str];
+}
