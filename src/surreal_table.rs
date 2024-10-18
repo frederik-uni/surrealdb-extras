28a703c29602bef53aad36900eb30253cc3e29c3
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2024-10-18 19:14:52 +0200

    add will consume now

--- src/surreal_table.rs
+++ src/surreal_table.rs
@@ -13,7 +13,7 @@ pub type Register = (F1, F1, F3);
 
 /// usefull functions for db
 /// will be created by proc macro
-pub trait SurrealTableInfo: Serialize + SurrealSelectInfo + Clone {
+pub trait SurrealTableInfo: Serialize + SurrealSelectInfo + Clone + 'static {
     /// db name
     fn name() -> &'static str;
     /// path to struct
@@ -59,25 +59,19 @@ pub trait SurrealTableInfo: Serialize + SurrealSelectInfo + Clone {
     }
 
     /// adds itself to the db and returns Record
-    async fn add_i<D: Connection>(
-        &'static self,
-        conn: &Surreal<D>,
-    ) -> Result<Option<Record>, surrealdb::Error> {
-        let r: Option<Vec<Record>> = conn.create(Self::name()).content(self).await?;
-        if let Some(mut v) = r {
-            return Ok(Some(v.remove(0)));
-        }
-        Ok(None)
+    async fn add_i<D: Connection>(self, conn: &Surreal<D>) -> Result<Record, surrealdb::Error> {
+        let mut r: Vec<Record> = conn.insert(Self::name()).content(self).await?;
+        return Ok(r.remove(0));
     }
 
     /// checks if item exists(adds to db if its not in db) and returns id
     async fn get_or_insert<C: Connection>(
-        &'static self,
+        self,
         db: &Surreal<C>,
-    ) -> Result<Option<Record>, surrealdb::Error> {
+    ) -> Result<Record, surrealdb::Error> {
         let check = self.check_if_exists(db).await?;
         if let Some(check) = check {
-            Ok(Some(check))
+            Ok(check)
         } else {
             self.add_i(db).await
         }
@@ -102,20 +96,14 @@ pub trait SurrealTableInfo: Serialize + SurrealSelectInfo + Clone {
     }
 
     /// adds itself to the db and returns true if there was a response
-    async fn add_s<D: Connection>(
-        &'static self,
-        conn: &Surreal<D>,
-    ) -> Result<bool, surrealdb::Error> {
-        let r: Option<Vec<Record>> = conn.create(Self::name()).content(self).await?;
-        Ok(match r {
-            Some(v) => !v.is_empty(),
-            None => false,
-        })
+    async fn add_s<D: Connection>(self, conn: Surreal<D>) -> Result<bool, surrealdb::Error> {
+        let r: Vec<Record> = conn.insert(Self::name()).content(self).await?;
+        Ok(!r.is_empty())
     }
 
     /// inserts itself to the db and returns true if there was a response
     async fn insert_s<D: Connection>(
-        &'static self,
+        self,
         conn: &Surreal<D>,
         id: RecordIdKey,
     ) -> Result<bool, surrealdb::Error> {
